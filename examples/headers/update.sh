8292abc5ce53349b733b45f3c87780c3a4026704
william <myname@example.com>
2026-04-13 19:47:22 +0800

    解析下如何更新libbpf 头部文件

--- examples/headers/update.sh
+++ examples/headers/update.sh
@@ -13,6 +13,7 @@ headers=(
     "$prefix"/src/bpf_tracing.h
 )
 
+#下载（curl）指定版本的 libbpf 源码压缩包，并只解压我们需要的头文件到当前目录（tar 的 --xform 选项用来把路径裁剪掉，只保留文件名），这样我们获得了与 libbpf 版本完全匹配的头文件。
 # Fetch libbpf release and extract the desired headers
 curl -sL "https://github.com/libbpf/libbpf/archive/refs/tags/v${LIBBPF_VERSION}.tar.gz" | \
     tar -xz --xform='s#.*/##' "${headers[@]}"
